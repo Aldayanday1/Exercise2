2eeaf1d1a34a811d184f7586b9221903ab89ff28
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-11-04 16:18:56 +0700

    Add void display dengan output console writeline

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -88,5 +88,18 @@ namespace QuickShort
             q_sort(j + 1, high);
 
         }
+        void display()
+        {
+            Console.WriteLine("\n---------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-----------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of data comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements:" + mov_count);
+        }
+
     }
 }
